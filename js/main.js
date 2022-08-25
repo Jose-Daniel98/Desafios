b3d9c3e31752035c04e16b46ec291f8347376b07
José Parra Zúñiga <parrazunigajosedaniel87@gmail.com>
2022-08-25 16:47:09 -0600

    Commit desafio de simulador interactivo

--- js/main.js
+++ js/main.js
@@ -1,83 +1,104 @@
-//Desafío Complementario
-//Crear un algoritmo utilizando un ciclo
+//Desafío #2
+//Simulador Interactivo
 
-/* Elaborar un algoritmo que obtenga el promedio de estatura para un grupo determinado de personas, dicho número de individuos es
-desconocido.
-El ciclo debe realizarse siempre y cuando se registre una estatura, es decir el número de estaturas debe ser mayor a 0 (cero)*/
+/* Dar por pantalla un menú que tenga las siguientes opciones: 1. Calcular el factorial de un número. 2. El promedio de una
+calificación. 3. Conversor de grados: Celsuis a Farenheit. 4. Lista de números pares e impares.*/
 
-
-let estatura = parseInt(prompt("Ingrese la estatura:"));
-let cantidadPersonas = 0; //Variable de cantidad de personas
-let sumaEstatura = 0; //Variable de estaturas de las personas
-let resultado;
-
-while (estatura > 0) {
-    estatura = parseInt(prompt("Ingrese la estatura:"));
-    cantidadPersonas = cantidadPersonas + 1;
-    sumaEstatura = sumaEstatura + estatura;
-    resultado = (sumaEstatura/cantidadPersonas);
+//1- FACTORIAL DE UN NUMERO
+function factorial(numero){
+    let numeroFactorial = 1;
+    for (let i = 1; i <= numero; i++) {
+        numeroFactorial = numeroFactorial * i;
+    }
+    return numeroFactorial;
 }
 
-if (cantidadPersonas === 0) {
-    console.log("No se ingresaron estaturas.");
-}else{
-    console.log("El promedio de estaturas de las " + cantidadPersonas + " personas es de: " + resultado +
-    " centrimetros.");
+//PROMEDIO DE CALIFICACIONES
+function calcularPromedio() {
+    let total = 0;
+    let calificacionFinal = 0;
+
+    for (let i = 1; i <= 4; i++){
+        let calificacionMateria = prompt("Ingrese la calificación de la materia: " + i);
+        let nuevaCalificacion = parseInt(calificacionMateria);
+        total = total + parseInt(nuevaCalificacion);
+    }
+
+    calificacionFinal = parseInt(total)/4;
+    alert("La suma de las calificaciones es: " + total +"."+ " Calificación final: " + calificacionFinal);
+    if (calificacionFinal >= 9) {
+        nota = "Excelente";
+    }else if(calificacionFinal >= 8){
+        nota = "Muy bien";
+    }else if(calificacionFinal >= 7){
+        nota = "Bien";
+    }else if(calificacionFinal >= 6){
+        nota = "Regular";
+    }else{
+        nota = "Reprobado";
+    }
+    return(calificacionFinal);
 }
 
+//CONVERSOR DE GRADOS: CELSIUS A FARENHEIT
+function conversorGradFaren(tipo, gradosFarenheitCelsius){
+    let grados = 0;
+    if(tipo == "Celsius"){
+        grados = (gradosFarenheitCelsius - 32)/1.8;
+    }else{
+        grados = gradosFarenheitCelsius * 1.8 + 32;
+    }
+    return grados;
+}
 
-/* Un programa para conocer en sueldo total de un trabajador, de acuerdo a su área, tomando en consideración los descuentos de renta,
-jubilacion e seguro.
-Antes de realizar los calculos, el sistema debe permitir un número que se asocie al área en la que labora. Las áreas son Ingenieros,
-Docentes, Gerentes, Otras profesiones */
+//NUMEROS PARES E IMPARES
+function impares(numero, tipo){
+    let i = 1;
+    let par = numero/2;
+    if (tipo == 1){
+        alert("Ingrese al 1- Impares");
+        while (i <= numero) {
+            let impar = i % 2;
+            if(impar != 0){
+                alert("Número impar: " + i);
+            }
+            i++;
+        }
+    }else{
+        alert("Ingrese al 0- Pares");
+        while (i <= par) {
+            let numeroPar = i*2;
+            alert(`Número par:  ${numeroPar}`);
+            i++;
+        }
+    }
+    return;
+}
 
-
-let sueldoIngeniero = 1200;
-let sueldoDocente = 800;
-let sueldoGerente = 900;
-let otrasProfesiones = 400;
-
-let renta = 0.1; //(10%) 10/100
-let jubilacion= 0.0725; //(7.25%) 7.25/100
-let seguro = 0.0625; //(6.25%) 6.25/100
-
-let numero = parseInt(prompt("Ingrese un número del 1 al 4" + 
-"\n 1 para Ingeniero" + 
-"\n 2 para Docente" + 
-"\n 3 para Gerente" + 
-"\n 4 para Otras profesiones"));
-
-if (numero == 1){
-    let nrenta = sueldoIngeniero * renta;
-    let njubilacion = sueldoIngeniero * jubilacion;
-    let nseguro = sueldoIngeniero * seguro;
-    let totalpagar = sueldoIngeniero - nrenta - njubilacion - nseguro;
-    console.log("El sueldo total del Ingeniero es: " + totalpagar + " colones CRC");
-
-}else if(numero == 2){
-
-    let nrenta = sueldoDocente * renta;
-    let njubilacion = sueldoDocente * jubilacion;
-    let nseguro = sueldoDocente * seguro;
-    let totalpagar = sueldoDocente - nrenta - njubilacion - nseguro;
-    console.log("El sueldo total del Docente es: " + totalpagar + " colones CRC");
-
-}else if(numero == 3){
-
-    let nrenta = sueldoGerente * renta;
-    let njubilacion = sueldoGerente * jubilacion;
-    let nseguro = sueldoGerente * seguro;
-    let totalpagar = sueldoGerente - nrenta - njubilacion - nseguro;
-    console.log("El sueldo total del Gerente es: " + totalpagar + " colones CRC");
-    
-}else if(numero == 4){
-
-    let nrenta = otrasProfesiones * renta;
-    let njubilacion = otrasProfesiones * jubilacion;
-    let nseguro = otrasProfesiones * seguro;
-    let totalpagar = otrasProfesiones - nrenta - njubilacion - nseguro;
-    console.log("El sueldo total de OStras profesiones es: " + totalpagar + " colones CRC");
-
-}else{
-    console.log("No se ha ingresado un número válido. ¡Vuelve a intentarlo!");
+//PROGRAMA CON MENU
+let opcion = 0;
+let nota = " ";
+while (opcion != 5) {
+    opcion = prompt("Menú Principal:\n1- Calcular el factorial de un número.\n2- El promedio de una calificación\n3- Conversor de grados: Celsuis a Farenheit\n4- Lista de números pares e impares.\n5- Salir.\nIngrese una opción: ");
+    if (opcion == 1) {
+        numero = prompt("Ingrese un número, para poder calcular el factorial: ");
+        numeroFactorial = factorial(numero);
+        alert(`El factorial del número ingresado es: ${numeroFactorial}`);
+    }else if (opcion == 2) {
+        calificacionFinal = calcularPromedio();
+        alert(`El promedio final es: ${calificacionFinal} + corresponde a: ${nota}`);
+    }else if(opcion == 3) {
+        let tipo = prompt("Desea convertir a: F. Farenheit / C. Celsius.");
+        let gradosFarenheitCelsius = prompt("Ingrese los grados: ");
+        let grados = conversorGradFaren(tipo, gradosFarenheitCelsius);
+        alert(`La conversión a grados es: + ${gradosFarenheitCelsius} ${tipo} + es:  + ${grados}`);
+    }else if(opcion == 4) {
+        let tipo = prompt("Desea imprimir los números: 0. Pares / 1. Impares.");
+        let numero = prompt("Cuantos números de ingresar?: ");
+        impares(numero, tipo);
+    }else if(opcion == 5){
+        alert("¡Gracias por su tiempo! ¡Vuelva pronto!");
+    }else{
+        alert("Opción inválida, verifique e intente de nuevo.");
+    }
 }
