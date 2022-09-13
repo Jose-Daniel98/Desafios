003722e6302a2f9e4f387edfc2c0cc59e53abd88
José Parra Zúñiga <parrazunigajosedaniel87@gmail.com>
2022-09-13 12:42:46 -0600

    Agregado desafio entregable de eventos

--- js/main.js
+++ js/main.js
@@ -1,67 +1,85 @@
-//Interactuar con el HTML
+//Eventos
 
-const h2 = document.getElementById("h2");
-const parrafo = document.getElementsByTagName("p");
-const parrafoClases = document.getElementsByClassName("parrafo");
-const contenedor= document.getElementById('contenedor');
-const input= document.getElementById('ingreso');
-const ul = document.getElementById('lista');
-const texto = document.createElement('p');
+let selectedRow = null;
 
-//innerText
-parrafo[0].innerText="Estoy estudiando Javascript"
-parrafo[1].innerText="Debo interactuar con el HTML"
-parrafo[2].innerText="Para cumplir con el desafio"
+//Alertas
+function showAlert(message, className){
+    const div = document.createElement("div");
+    div.className = `alert alert-${className}`;
+    
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector(".container");
+    const main = document.querySelector(".main");
+    container.insertBefore(div, main);
 
-h2.innerText= "Desafío: Interactuar con HTML"
-texto.innerText = "Lista de los productos";
-
-//style
-texto.style.color = "blue";
-
-
-contenedor.append(texto);
-
-const servicios = [
-  { id: 1, nombre: "Lirio", precio: 1200},
-  { id: 2, nombre: "Rosa", precio: 1800},
-  { id: 3, nombre: "Tulipan", precio: 2800},
-  { id: 4, nombre: "Begonia", precio: 1500},
-  { id: 5, nombre: "Hortensia", precio: 1200},
-  { id: 6, nombre: "Orquidea", precio: 1900},
-];
-
-for (const servicio of servicios) {
-
-  let li = document.createElement('li')
-
-  li.innerHTML=`
-  <h3>${servicio.nombre}</h3>
-  <p>${servicio.precio}</p>
-  <br />
-  </div>
- `
-  ul.append(li)
+    setTimeout(() => document.querySelector("alert").remove(), 3000);
 }
 
-solicitarNombre();
-solicitarInformación();
-
-// Ingresar datos por pantalla
-function solicitarNombre() {
-  let nombre = prompt("Ingrese su nombre");
-  while(nombre === "")
-  nombre = prompt("Ingrese su nombre");
-  let apellido = prompt("ingrese su apellido");
-  while(apellido === "")
-  apellido = prompt("Ingrese su apellido");
-  document.write("Hola " + nombre + " " + apellido + "!");
+//Limpiar todos los campos
+function clearFields(){
+    document.querySelector("#firstName").value = "";
+    document.querySelector("#lastName").value = "";
+    document.querySelector("#rollNo").value = "";
 }
 
-function solicitarInformación() {
-  let informacion = prompt("Ingrese lo que guste escribir: ")
-  while(informacion === "")
-  informacion = prompt("Ingrese lo que guste escribir:");
-  console.log("Lo que escribió fue: " + informacion);
-  document.write("Ingrese un mensaje para mostar en el html: " + informacion);
-}
+//Agregar información
+document.querySelector("#student-form").addEventListener("submit", (e) =>{
+    e.preventDefault();
+
+    //Tomar los valores del form
+    const firstName = document.querySelector("#firstName").value;
+    const lastName = document.querySelector("#lastName").value;
+    const rollNo = document.querySelector("#rollNo").value;
+
+    //Validaciones
+    if(firstName == "" || lastName == "" || rollNo == ""){
+        showAlert("Por favor llena todos los campos", "danger");
+    }
+    else{
+        if(selectedRow == null){
+            const list = document.querySelector("#student-list");
+            const row = document.createElement("tr");
+
+            row.innerHTML = `
+                <td>${firstName}</td>
+                <td>${lastName}</td>
+                <td>${rollNo}</td>
+                <td>
+                <a href="#" class="btn btn-warning btn-sm edit">Edit</a>
+                <a href="#" class="btn btn-danger btn-sm delete">Delete</a>
+            `;
+            list.appendChild(row);
+            selectedRow = null;
+            showAlert("Información agregada", "success");
+        }
+        else{
+            selectedRow.children[0].textContent = firstName;
+            selectedRow.children[1].textContent = lastName;
+            selectedRow.children[2].textContent = rollNo;
+            selectedRow = null;
+            showAlert("Información editada", "info");
+        }
+
+        clearFields();
+    }
+});
+
+//Editar información
+document.querySelector("#student-list").addEventListener("click", (e) =>{
+    target = e.target;
+    if(target.classList.contains("edit")){
+        selectedRow = target.parentElement.parentElement;
+        document.querySelector("#firstName").value = selectedRow.children[0].textContent;
+        document.querySelector("#lastName").value = selectedRow.children[1].textContent;
+        document.querySelector("#rollNo").value = selectedRow.children[2].textContent;
+    }
+});
+
+//Eliminar Información
+document.querySelector("#student-list").addEventListener("click", (e) =>{
+    target = e.target;
+    if(target.classList.contains("delete")){
+        target.parentElement.parentElement.remove();
+        showAlert("Información eliminada", "danger");
+    }
+});
